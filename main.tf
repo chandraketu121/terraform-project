db430b3969e46ed9a418ca7c5f73dd3cec0027b0
chandraketu121 <124147471+chandraketu121@users.noreply.github.com>
2023-05-31 15:45:57 +0530

    Update maain.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 #this file consists of code for instances and sg
 provider "aws" {
-region = "us-east-1"
-access_key = "AKIATZ77G64VDLCDBEMS"
-secret_key = "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"
+region = "ap-south-1"
+access_key = "AKIATYLWZMNG7G6Y23EM"
+secret_key = "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"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-03c7d01cf4dedc891"
+  ami             = "ami-0607784b46cbe5816"
   instance_type   = "t2.micro"
-  key_name        = "rahamnewkp01"
+  key_name        = "730ambatch"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1a"
+  availability_zone = "ap-south-1a"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
@@ -25,11 +25,11 @@ EOF
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-03c7d01cf4dedc891"
+  ami             = "ami-0607784b46cbe5816"
   instance_type   = "t2.micro"
-  key_name        = "rahamnewkp01"
+  key_name        = "730ambatch"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1b"
+  availability_zone = "ap-south-1b"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
@@ -44,22 +44,22 @@ EOF
 }
 
 resource "aws_instance" "three" {
-  ami             = "ami-03c7d01cf4dedc891"
+  ami             = "ami-0607784b46cbe5816"
   instance_type   = "t2.micro"
-  key_name        = "rahamnewkp01"
+  key_name        = "730ambatch"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1a"
+  availability_zone = "ap-south-1a"
   tags = {
     Name = "app-server-1"
   }
 }
 
 resource "aws_instance" "four" {
-  ami             = "ami-03c7d01cf4dedc891"
+  ami             = "ami-0607784b46cbe5816"
   instance_type   = "t2.micro"
-  key_name        = "rahamnewkp01"
+  key_name        = "730ambatch"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1b"
+  availability_zone = "ap-south-1b"
   tags = {
     Name = "app-server-2"
   }
@@ -90,15 +90,15 @@ resource "aws_security_group" "five" {
 }
 
 resource "aws_s3_bucket" "six" {
-  bucket = "rahamshaikterra77889900"
+  bucket = "chandra121"
 }
 
 resource "aws_iam_user" "seven" {
-name = "rahamuser11" 
+name = "chandra1111" 
 }
 
 resource "aws_ebs_volume" "eight" {
- availability_zone = "us-east-1a"
+ availability_zone = "ap-south-1a"
   size = 40
   tags = {
     Name = "ebs-001"
